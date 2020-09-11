b984cd9d3f47d1c23dc9b188f95bb34e90d13abe
Nick Paavola <nick.paavola@gmail.com>
2020-09-10 21:53:52 -0500

    ex 5.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState([])
+  const [password, setPassword] = useState([])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +15,85 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      
+    } catch (exception) {
+      console.log('login error')
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    try {
+      console.log('signed out', user.name)
+      window.localStorage.removeItem('loggedBlogAppUser')
+      setUser(null)
+    } catch (exception) {
+      console.log('logout error')
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>
+        password
+        <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+      </div>
+      <button type="submit">log in</button>
+    </form>
   )
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to blog application</h2>
+        {loginForm()}
+      </div>
+    )  
+  } else {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} is logged in</p>
+        <form onSubmit={handleLogout}>
+          <button type="submit" >log out</button>
+        </form>
+
+        <h3>list of blogs</h3>
+        
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
