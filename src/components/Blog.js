0f786ea66cbb874b4e447966c8a5d594c5f9e4f6
Nick Paavola <nick.paavola@gmail.com>
2020-09-18 21:01:15 -0500

    ex 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,47 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleDisplay = () => {
+    setVisible(!visible)
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title}
+          <button onClick={toggleDisplay}>hide</button>
+        </div>
+        <div>
+          {blog.author}
+        </div>
+        <div>
+          Likes: {blog.likes}
+          <button onClick={() => {console.log('liked!')}}>like</button>
+        </div>
+        <div>
+          {blog.url}
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} 
+        <button onClick={toggleDisplay}>view</button>
+      </div>
+    )
+  }
+}
 
 export default Blog
