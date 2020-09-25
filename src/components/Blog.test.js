7a29fc0f7bc2212f9cd04ac0058341e687ea5068
Nick Paavola <nick.paavola@gmail.com>
2020-09-24 20:04:50 -0500

    ex 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('blog renders title and author but not url or likes', () => {
+describe('<Blog />', () => {
+  let component
   const blog = {
     title: 'test blog',
     author: 'nick',
@@ -11,25 +12,27 @@ test('blog renders title and author but not url or likes', () => {
     likes: 99
   }
 
-  const component = render(
-    <Blog blog={blog} />
-  )
+  beforeEach(() => {
+    component = render(
+      <Blog blog={blog} />
+    )
+  })
 
-  component.debug()
+  test('blog renders title and author but not url or likes', () => {
+    const titleAuthor = component.container.querySelector('.titleAuthor')
+    const url = component.container.querySelector('.url')
+    const likes = component.container.querySelector('.likes')
 
-  const titleAuthor = component.container.querySelector('.titleAuthor')
-  const url = component.container.querySelector('.url')
-  const likes = component.container.querySelector('.likes')
+    expect(titleAuthor).toBeDefined()
+    expect(url).toBeNull()
+    expect(likes).toBeNull()
+  })
 
-  expect(titleAuthor).toBeDefined()
-  expect(url).toBeNull()
-  expect(likes).toBeNull()
+  test('blog renders url and likes when expanded', () => {
+    const url = component.container.querySelector('.url')
+    const likes = component.container.querySelector('.likes')
 
-  // expect(component.container).not.toHaveTextContent(
-  //   'xyz.com'
-  // )
-
-  // expect(component.container).not.toHaveTextContent(
-  //   '99'
-  // )
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
+  })
 })
